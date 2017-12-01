db19b06551a82519c40858de2968737a0379583a
Lei Zhang <antiagainst@gmail.com>
2017-12-01 17:59:58 -0500

    Refresh shaderc and its dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1054,7 +1054,7 @@ void main() {
     static VOID_MAIN_ASSEMBLY: &'static str = "\
 ; SPIR-V
 ; Version: 1.0
-; Generator: Google Shaderc over Glslang; 1
+; Generator: Google Shaderc over Glslang; 2
 ; Bound: 6
 ; Schema: 0
                OpCapability Shader
