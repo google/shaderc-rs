94ed68b93fa31fbc78ce72ed1d154dd1f4179bfc
Lei Zhang <antiagainst@gmail.com>
2018-01-03 19:24:17 -0500

    Refresh native projects

--- src/lib.rs
+++ src/lib.rs
@@ -1075,7 +1075,7 @@ void main() {
     static VOID_MAIN_ASSEMBLY: &'static str = "\
 ; SPIR-V
 ; Version: 1.0
-; Generator: Google Shaderc over Glslang; 2
+; Generator: Google Shaderc over Glslang; 3
 ; Bound: 6
 ; Schema: 0
                OpCapability Shader
